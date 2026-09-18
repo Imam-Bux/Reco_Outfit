38bd3343e4d67d29613bad1468b41c37404d7e5a
Imam-Bux <imambatman12@gmail.com>
2026-09-19 04:04:01 +0500

    Fixing a few errors

--- backend/modules/receipt/shopInfo.js
+++ backend/modules/receipt/shopInfo.js
@@ -1,5 +1,5 @@
-export const SHOP_INFO = {
-  logo: process.env.SHOP_LOGO_URL || '',
+﻿export const SHOP_INFO = {
+  logo: process.env.SHOP_LOGO_URL || 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjAiIGhlaWdodD0iMTIwIiB2aWV3Qm94PSIwIDAgMTAwIDEwMCIgZmlsbD0ibm9uZSI+CiAgPGNpcmNsZSBjeD0iNTAiIGN5PSI1MCIgcj0iNDgiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZT0iIzAwMDAwMCIgc3Ryb2tlLXdpZHRoPSI1Ii8+CiAgPHRleHQgeD0iNTAiIHk9IjUwIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBkb21pbmFudC1iYXNlbGluZT0iY2VudHJhbCIgZmlsbD0iI0ZGRUYwMCIgZm9udC1mYW1pbHk9Ikdlb3JnaWEsIHNlcmlmIiBmb250LXdlaWdodD0iODAwIiBmb250LXNpemU9IjUyIiBsZXR0ZXItc3BhY2luZz0iMiI+Uk88L3RleHQ+Cjwvc3ZnPg==',
   name: 'RecoOutfit',
   address: 'Reco Outfitters near Reco Cleaner, Gulab Pan House, Jail Road, HYD',
   contact: '0316 3926832',
